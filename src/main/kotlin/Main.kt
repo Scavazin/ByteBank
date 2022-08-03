017f212ef215905357a02fd28ce1fc6fa490cbc5
Victor Scavazin <victor.scavazin@marsaude.net>
2022-08-03 00:54:29 -0300

    Creating classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 fun main() {
     println("Bem vindo ao Byte Bank")
     testaComportamentosConta()
+
 }
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0) println("Saldo positivo")
-    else if (saldo == 0.0) println("Saldo zero")
-    else println("Saldo negativo")
-}
+
 
---
+++ src/main/kotlin/testaCondicoes.kt
@@ -0,0 +1,5 @@
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0) println("Saldo positivo")
+    else if (saldo == 0.0) println("Saldo zero")
+    else println("Saldo negativo")
+}
